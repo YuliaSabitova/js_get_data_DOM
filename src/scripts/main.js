df5235b76c4e50e78495df1efe04c2d8e89135e0
YuliaSabitova <yuliasab2017@mail.com>
2025-05-26 13:41:58 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const spans = [...document.querySelectorAll('.population')];
+let total = 0;
+
+for (const span of spans) {
+  const text = span.textContent.split(',');
+  const str = Number(text.reduce((firstM, secondM) => firstM + secondM));
+
+  span.textContent = str;
+  total += str;
+}
+
+const average = Math.round(total / spans.length);
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-Us');
+
+document.querySelector('.average-population').textContent =
+  average.toLocaleString('en-Us');
+
